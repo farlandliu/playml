eac836ce90c7ef543e2970b9c7542d946f421a40
farland <farland@163.com>
2019-03-24 23:39:12 +0800

    :r for data science -01

--- r4ds/eda_r4ds.R
+++ r4ds/eda_r4ds.R
@@ -5,3 +5,14 @@ head(diamonds)
 summary(diamonds)
 dim(diamonds)
 ?diamonds
+
+library(nycflights13)
+library(tidyverse)
+
+?flights
+data("flights")
+head(flights)
+filter(flights, month==2, day==1)
+jan1 <- filter(flights, month == 1, day == 1)
+x <- filter(flights, is.na(dep_time)==TRUE)
+count(x)
